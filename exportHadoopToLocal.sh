c670b7559915ab274dfe405ac74e7f5711cf85e8
bubunzer <49228505+bubunzer@users.noreply.github.com>
2021-02-04 16:50:19 +0100

    Update exportHadoopToLocal.sh

--- exportHadoopToLocal.sh
+++ exportHadoopToLocal.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
-path=/home/sobun/ #Path du fichier à exporter
-hdfspath=/user/root/projet/ #Dossier d'import
+hdfspath=/user/root/projet/ #Dossier du fichier à exporter
+path=/home/sobun/ #Path du dossier d'import
 echo "What is the filename?"
 read filename
 hdfs dfs -get $hdfspath$filename $path 

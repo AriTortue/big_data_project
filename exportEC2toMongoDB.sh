534653f60ca1c798fcab48045109b598aec6bcdf
bubunzer <49228505+bubunzer@users.noreply.github.com>
2021-02-10 17:07:49 +0100

    Update exportEC2toMongoDB.sh

--- exportEC2toMongoDB.sh
+++ exportEC2toMongoDB.sh
@@ -1,3 +1,13 @@
 #! /bin/sh
+echo "Hostname?"
+read hostname
+echo "Database name?"
+read dbname
+echo "Collection name?"
+read cname
+echo "Path of file to export?"
+read filepath
+echo "What is the filename?"
+read filename
 
-mongoimport --host "hostname de l'ec2" --port 27017 --db "nom de la database" --collection "nom de la collection" --file "chemin d'accès du fichier à importer"
+mongoimport --host $hostname --port 27017 --db $dbname --collection $cname --file $filepath/$filename
